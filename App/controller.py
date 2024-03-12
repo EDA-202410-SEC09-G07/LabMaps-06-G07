c2212d8aee7a758da2aa36fe3f76e5be0b5d97c6
KarinXimenaGuerrero <k.guerreroh@uniandes.edu.co>
2024-03-11 20:07:25 -0500

    Correcciones

--- App/controller.py
+++ App/controller.py
@@ -63,7 +63,7 @@ def loadBooks(control):
     Carga los libros del archivo. Por cada libro se indica al
     modelo que debe adicionarlo al catalogo.
     """
-    booksfile = cf.data_dir + 'GoodReads/books.csv'
+    booksfile = cf.data_dir + 'GoodReads/books-small.csv'
     input_file = csv.DictReader(open(booksfile, encoding='utf-8'))
     for book in input_file:
         model.addBook(control['model'], book)
